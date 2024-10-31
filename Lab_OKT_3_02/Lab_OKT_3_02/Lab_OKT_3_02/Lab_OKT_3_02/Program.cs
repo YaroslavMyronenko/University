3c8dcb7d3ee140a75c53019b4e53fdca59c461d0
Yaroslav Myronenko <jarik2707@gmail.com>
2024-10-31 19:43:43 +0200

    lab 4 completed

---
+++ Lab_OKT_3_02/Lab_OKT_3_02/Lab_OKT_3_02/Lab_OKT_3_02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random nambers = new Random();
+        int rows = 10;
+        int cols = 10;
+        double[,] matrix = new double[rows, cols];
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                matrix[r, c] = Math.Round(nambers.NextDouble() * (99.0 - 10.0) + 10.0, 2);
+            }
+        }
+        Console.WriteLine("Початкова матриця : ");
+        Console.WriteLine();
+        for (int ro = 0; ro < rows; ro++)
+        {
+            for (int co = 0; co < cols; co++)
+            {
+                Console.Write(matrix[ro, co] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("");
+        Console.WriteLine("");
+        for (int r = 0; r < rows; r++)
+        {
+            int maxindex = 0;
+            int minindex = 0;
+            for (int c = 1; c < cols; c++)
+            {
+                if (matrix[r, c] > matrix[r, maxindex])
+                {
+                    maxindex = c;
+                }
+                if (matrix[r, c] < matrix[r, minindex])
+                {
+                    minindex = c;
+                }
+            }
+            double temp_max = matrix[r, 0];
+            matrix[r, 0] = matrix[r, maxindex];
+            matrix[r, maxindex] = temp_max;
+
+            double temp_min = matrix[r, cols - 1];
+            matrix[r, cols - 1] = matrix[r, minindex];
+            matrix[r, minindex] = temp_min;
+        }
+        Console.WriteLine("\nМодифікований масив");
+        PrintMatrix(matrix);
+    }
+    static void PrintMatrix(double[,] matrix)
+    {
+        for (int r = 0; r < matrix.GetLength(0); r++)
+        {
+            for (int c = 0; c < matrix.GetLength(1); c++)
+            {
+                Console.Write(matrix[r, c] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab_OKT_4/Lab_OKT_4/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    // Метод обміну елементів
+    static void Swap(ref int e1, ref int e2)
+    {
+        var temp = e1;
+        e1 = e2;
+        e2 = temp;
+    }
+
+    // Сортування змішуванням (шейкерне сортування)
+    static int[] ShakerSort(int[] arrayNumbers)
+    {
+        for (var i = 0; i < arrayNumbers.Length / 2; i++)
+        {
+            var swapFlag = false;
+            // Прохід зліва направо
+            for (var j = i; j < arrayNumbers.Length - i - 1; j++)
+            {
+                if (arrayNumbers[j] > arrayNumbers[j + 1])
+                {
+                    Swap(ref arrayNumbers[j], ref arrayNumbers[j + 1]);
+                    swapFlag = true;
+                }
+            }
+
+            // Прохід справа наліво
+            for (var j = arrayNumbers.Length - 2 - i; j > i; j--)
+            {
+                if (arrayNumbers[j - 1] > arrayNumbers[j])
+                {
+                    Swap(ref arrayNumbers[j - 1], ref arrayNumbers[j]);
+                    swapFlag = true;
+                }
+            }
+
+            // Якщо обмінів не було
+            if (!swapFlag)
+            {
+                break;
+            }
+        }
+        return arrayNumbers;
+    }
+
+    // Метод бінарного пошуку з використанням циклу
+    static int BinarySearch(int[] array, int searchedValue, out int comparisons)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+        comparisons = 0;
+
+        // Поки не зійшлись межі робочої зони масиву
+        while (left <= right)
+        {
+            comparisons++;
+            // Індекс середнього елементу
+            var middle = (left + right) / 2;
+
+            if (searchedValue == array[middle])
+            {
+                return middle;
+            }
+            else if (searchedValue < array[middle])
+            {
+                // Звуження робочої зони масиву справа
+                right = middle - 1;
+            }
+            else
+            {
+                // Звуження робочої зони масиву зліва
+                left = middle + 1;
+            }
+        }
+        // Нічого не знайдено
+        return -1;
+    }
+
+    static void Main()
+    {
+        int size = 20;
+        int[] testArray = new int[size];
+        Random rand = new Random();
+        Console.WriteLine("Початковий масив: -->");
+        for (int a = 0; a < testArray.Length; a++){
+            testArray[a] = rand.Next(0, 20);
+            Console.Write(testArray[a] + ", ");
+        }      
+        Console.WriteLine("\n=====================================================================");
+        ShakerSort(testArray); //Викликаю функцію для сортування
+        Console.WriteLine("Масив впорядкований шейкерним способом:  ");
+        foreach (int num in testArray)
+        {
+            Console.Write(num + ", ");
+        }
+            Console.WriteLine("\n=====================================================================");
+        // Введення випадкового шуканого числа
+        int wanted_number = rand.Next(0, 20); // випадкове число для пошуку в масиві
+        int comparisons;
+        
+        // Бінарний пошук невідомого числа
+        int resultIndex = BinarySearch(testArray, wanted_number, out comparisons);
+
+        // Виведення результату пошуку
+        if (resultIndex != -1)
+        {
+            Console.WriteLine("Значення " + wanted_number + " знайдено :) на індексі " + resultIndex);
+        }
+        else
+        {
+            Console.WriteLine("Значення " + wanted_number + " не знайдено в масиві :(");
+        }
+        Console.Write("Кількість порівнянь під час бінарного пошуку: " + comparisons);
+        Console.WriteLine("\n=====================================================================");   
+    }
+}
